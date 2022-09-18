593babfd6e82b496abba1bb444f9332e53709ea1
Mike Rapa <mike@mikerapa.com>
2022-09-17 23:54:35 -0400

    initial version

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+List<Dictionary<string, object>> getWorldCupData(){
+    var sr = new StreamReader(@"worldcupdata.json");
+    var jsonString = sr.ReadToEnd();
+    var cupWinnerDict = System.Text.Json.JsonSerializer.Deserialize<List<Dictionary<string, object>>>(jsonString);
+    return cupWinnerDict;
+}
+
+void printData(List<Dictionary<string, object>> worldCupList){
+    foreach (Dictionary<string, object> worldCup in worldCupList){
+        Console.WriteLine($"{worldCup["country"]} - {worldCup["year"]}" );
+    }
+}
+
+var worldcupdata = getWorldCupData();
+printData(worldcupdata);
+// foreach (Dictionary<string, object> worldCup in worldcupdata){
+//     Console.WriteLine(worldCup["country"]);
+// }
+
+var g = worldcupdata.Where(wc=>wc["country"].Equals("Germany"));
+
